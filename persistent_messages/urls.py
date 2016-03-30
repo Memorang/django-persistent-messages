585b237421fbd58c7c847df1da03f722c67519b1
George Courtsunis <gjcourt@gmail.com>
2016-03-29 18:09:24 -0700

    Update url paths to correct use view argument

--- persistent_messages/urls.py
+++ persistent_messages/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
+from persistent_messages import views
+
 
 urlpatterns = [
-    url(r'^detail/(?P<message_id>\d+)/$', 'persistent_messages.views.message_detail', name='message_detail'),
-    url(r'^mark_read/(?P<message_id>\d+)/$', 'persistent_messages.views.message_mark_read', name='message_mark_read'),
-    url(r'^mark_read/all/$', 'persistent_messages.views.message_mark_all_read', name='message_mark_all_read'),
+    url(r'^detail/(?P<message_id>\d+)/$', views.message_detail, name='message_detail'),
+    url(r'^mark_read/(?P<message_id>\d+)/$', views.message_mark_read, name='message_mark_read'),
+    url(r'^mark_read/all/$', views.message_mark_all_read, name='message_mark_all_read'),
 ]
