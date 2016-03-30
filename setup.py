15538e43af7b4c6ff749c14961eebc96f400ed2f
George Courtsunis <gjcourt@gmail.com>
2016-03-29 23:19:47 -0700

    Migrate to setuptools from distutils

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 import os
-from distutils.core import setup
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from setuptools import (
+    find_packages,
+    setup,
+)
 
 
 setup(
     name='django-persistent-messages',
+    author='philomat',
+    maintainer='gjcourt',
     version='0.1.1',
     description='A Django app for unified and persistent user messages/notifications, built on top of Django\'s messages framework',
-    long_description=read('README.md'),
-    author='philomat',
-    license='BSD',
+    long_description=open('README.md').read(),
+    license='MIT',
     url='http://github.com/memorang/django-persistent-messages',
     keywords=['messages', 'django', 'persistent',],
-    packages=[
-        'persistent_messages',
-    ],
+    packages=find_packages()
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -27,5 +25,6 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
+    include_package_data=True,
     zip_safe=False,
 )
