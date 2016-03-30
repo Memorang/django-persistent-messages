cc24aeec2fc832d96bffb3f743b3e06ac515ec41
George Courtsunis <gjcourt@gmail.com>
2016-03-29 18:12:04 -0700

    Update setup.py information

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import os
 from distutils.core import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
     name='django-persistent-messages',
-    version='0.1',
+    version='0.1.1',
     description='A Django app for unified and persistent user messages/notifications, built on top of Django\'s messages framework',
     long_description=read('README.md'),
     author='philomat',
     license='BSD',
-    url='http://github.com/philomat/django-persistent-messages',
-    keywords = ['messages', 'django', 'persistent',],
+    url='http://github.com/memorang/django-persistent-messages',
+    keywords=['messages', 'django', 'persistent',],
     packages=[
         'persistent_messages',
     ],
